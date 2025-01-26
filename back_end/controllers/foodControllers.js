9dff6da7c2bc89853522910a769fdc24f9c35d1b
Hasnaa Jinni <hassnajinni@gmail.com>
2025-01-26 17:00:38 +0100

    updates

--- back_end/controllers/foodControllers.js
+++ back_end/controllers/foodControllers.js
@@ -5,6 +5,24 @@ import fs from 'fs'
 //add food item
 
 const addFood = async(req,res)=>{
+     
+    let image_filename = `${req.file.filename}`;
+
+    const food = new foodModel({
+        name:req.body.name,
+        description:req.body.description,
+        price:req.body.price,
+        category:req.body.category,
+        image:image_filename
+    })
+
+    try{
+        await food.save()
+        res.json({success:true,message:"food added"})
+    }
+    catch(err){
+       console.log(err)
+    }
 
 }
 
--- back_end/routes/foodRoute.js
+++ back_end/routes/foodRoute.js
@@ -18,7 +18,7 @@ const storage = multer.diskStorage({
 const upload = multer({storage:storage})
 
 
-foodRouter.post("/add",addFood)
+foodRouter.post("/add",upload.single("image"),addFood)
 
 
 
